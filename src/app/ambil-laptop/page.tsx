0f7b68291ce3b589a436419a2493076084fd2aa1
Muhammad Hamka <hamkarifai49@gmail.com>
2025-08-25 11:47:51 +0700

    menambahkan label di setiap page

--- src/app/ambil-laptop/page.tsx
+++ src/app/ambil-laptop/page.tsx
@@ -3,11 +3,13 @@ import CameraPage from "../components/Camera";
 export default function AmbilLaptopPage() {
   return (
     <>
-      <>
-        <main className="w-full items-center">
-          <CameraPage />
-        </main>
-      </>
+      <main className="relative w-full h-screen">
+        <CameraPage />
+
+        <h1 className="absolute top-6 left-1/2 -translate-x-1/2 text-white text-2xl font-bold bg-black/50 px-4 py-2 rounded-lg">
+          Ambil Laptop 💻
+        </h1>
+      </main>
     </>
   );
 }
